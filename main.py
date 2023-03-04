e057b761ac9b2ce308fe563ae6410994f4b618d8
Michael Primavera <pastasauce59@gmail.com>
2023-03-03 20:51:59 -0500

    created the rest of the elif-else statement for report of resources and finally processing the customer's entire order using the methods from Menu, CoffeeMachine and MoneyMachine by checking for sufficent resources, then if true making payment and then if true making the coffee and reducing resources on hand for coffee machine.

--- main.py
+++ main.py
@@ -9,11 +9,18 @@ coffee_machine = CoffeeMaker()
 menu = Menu()
 machine_on = True
 
-coffee_machine.report()
-money_machine.report()
 
 while machine_on:
     options = menu.get_items()
     choice = input(f"What would you like? ({options}): ")
     if choice == 'off':
         machine_on = False
+    elif choice == 'report':
+        coffee_machine.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_machine.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_machine.make_coffee(drink)
+        
