702cd2fb20b62db2c52bae041f5808c290677873
Michael Primavera <pastasauce59@gmail.com>
2023-03-03 20:10:07 -0500

    Incorrectly initiated classes, now created variables from the classes, printed reports, now creating while loop to offer user choice of drink.

--- main.py
+++ main.py
@@ -4,5 +4,16 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-CoffeeMaker.__init__(CoffeeMaker)
-CoffeeMaker.report(CoffeeMaker)
+money_machine = MoneyMachine()
+coffee_machine = CoffeeMaker()
+menu = Menu()
+machine_on = True
+
+coffee_machine.report()
+money_machine.report()
+
+while machine_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == 'off':
+        machine_on = False
